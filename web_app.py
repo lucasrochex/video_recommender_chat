783aa24a461a11fbf900e3e692c25b3ba842af12
lucasrochex <37813005+lucasrochex@users.noreply.github.com>
2024-11-05 09:23:08 -0300

    Add files via upload

---
+++ web_app.py
@@ -0,0 +1,29 @@
+from src.rag.chains import get_video_recommendation
+import streamlit as st
+
+st.title("Recomendador de video")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("O que busca hoje?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    #response = f"Echo: {prompt}"
+    response = get_video_recommendation(prompt)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
+    print(st.session_state.messages)
